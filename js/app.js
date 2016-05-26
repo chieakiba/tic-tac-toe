ebcf1c3a767945309502f7611b95d58cdb7fcc0f
Chie <akiba.chie@gmail.com>
2016-05-25 18:27:58 -0700

    Added play again button function and solved how to make the winner function work. Also added feedback and play again button css styles

--- js/app.js
+++ js/app.js
@@ -5,63 +5,36 @@ var counter = 0;
 var randomNumber;
 var randomSquare;
 var button = $('.play-again');
-var board = [{
-    cell: 1,
-    point: 2,
-}, {
-    cell: 2,
-    point: 7,
-}, {
-    cell: 3,
-    point: 6,
-}, {
-    cell: 4,
-    point: 9,
-}, {
-    cell: 5,
-    point: 5,
-}, {
-    cell: 6,
-    point: 1,
-}, {
-    cell: 7,
-    point: 4,
-}, {
-    cell: 8,
-    point: 3,
-}, {
-    cell: 9,
-    point: 8,
-}];
 
 //function to determine winner
 function winner() {
-        if ($('.first').text() == cross && $('.second').text() == cross && $('.third').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.fourth').text() == cross && $('.fifth').text() == cross && $('.sixth').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.seventh').text() == cross && $('.eighth').text() == cross && $('.ninth').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.first').text() == cross && $('.fifth').text() == cross && $('.ninth').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.third').text() == cross && $('.fifth').text() == cross && $('.seventh').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.first').text() == cross && $('.fourth').text() == cross && $('.seventh').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.third').text() == cross && $('.sixth').text() == cross && $('.ninth').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } else if ($('.second').text() == cross && $('.fifth').text() == cross && $('.eighth').text() == cross) {
-            $('.feedback').append('Congratulations! You won!');
-            button.show();
-        } 
-
+    if ($('.first').text() == cross && $('.second').text() == cross && $('.third').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.fourth').text() == cross && $('.fifth').text() == cross && $('.sixth').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.seventh').text() == cross && $('.eighth').text() == cross && $('.ninth').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.first').text() == cross && $('.fifth').text() == cross && $('.ninth').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.third').text() == cross && $('.fifth').text() == cross && $('.seventh').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.first').text() == cross && $('.fourth').text() == cross && $('.seventh').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.third').text() == cross && $('.sixth').text() == cross && $('.ninth').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else if ($('.second').text() == cross && $('.fifth').text() == cross && $('.eighth').text() == cross) {
+        $('.feedback').append('Congratulations! You won!');
+        button.show();
+    } else {
+        return false;
+    }
 }
 
 //function that randomly puts circles in the cell
@@ -69,27 +42,42 @@ function playerAI() {
     do {
         randomNumber = Math.floor((Math.random() * 9) - 1);
         randomSquare = $('.square').eq(randomNumber);
+        if (counter == 9) {
+            return;
+        }
     } while (randomSquare.children().text() != '' && counter < 9)
     randomSquare.children().append(circle);
     counter++;
 }
 
+//play again button function
+var playAgain = function() {
+    button.click(function() {
+        $('.square').children().empty();
+        $('.feedback').empty();
+        button.hide();
+        counter = 0;
+        game();
+    })
+}
 
-$(document).ready(function() {
-    button.hide();
-    //function that allows player to put the cross on the cell they click
+//function that allows player to start game
+var game = function() {
     $('.square').one("click", function() {
         $(this).children().append(cross);
         counter++;
-        if (counter == 9) {
-            return;
-        }
         playerAI();
         winner();
         if (counter == 9 && !winner()) {
-            $('.feedback').append('Cat\s Game!');
+            $('.feedback').append('Cat\'s Game!');
             button.show();
         }
+        return;
     })
+}
 
+$(document).ready(function() {
+    button.hide();
+    game();
+    playAgain();
 });
